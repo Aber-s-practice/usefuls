f571d378cfc791266e5acfb77463bfddfa58dd25
abersheeran <me@abersheeran.com>
2021-12-22 01:52:11 +0000

    DNS 查询允许指定请求方式和超时时间

--- usefuls/dns.py
+++ usefuls/dns.py
@@ -28,9 +28,19 @@ DNS_TYPE = [
 ]
 
 
-async def query(nameserver: str, domain: str, dns_type: str) -> dns.message.Message:
+async def query(
+    method: str, nameserver: str, timeout: float, domain: str, dns_type: str
+) -> dns.message.Message:
     query = dns.message.make_query(domain, dns_type)
-    result = await dns.asyncquery.udp(query, nameserver)
+    if method == "udp":
+        func = dns.asyncquery.udp_with_fallback
+    elif method == "tcp":
+        func = dns.asyncquery.tcp
+    elif method == "tls":
+        func = dns.asyncquery.tls
+    elif method == "https":
+        func = dns.asyncquery.https
+    result = await func(query, nameserver, timeout=timeout)
     return result
 
 
@@ -41,27 +51,46 @@ def display_result(result: dns.message.Message) -> None:
 
 @click.command()
 @click.option("-NS", "--nameserver", help="DNS Server")
+@click.option(
+    "-M",
+    "--method",
+    type=click.Choice(["udp", "tcp", "tls", "https"], case_sensitive=False),
+    default="udp",
+)
+@click.option("-T", "--timeout", type=float, default=3)
 @click.argument("domain")
 @click.argument(
     "dns-type",
-    type=click.Choice(DNS_TYPE + ["ALL"], case_sensitive=True),
+    type=click.Choice(DNS_TYPE + ["ALL"], case_sensitive=False),
     default="ALL",
 )
-def main(domain: str = None, dns_type: str = "ALL", nameserver: str = None):
+def main(
+    domain: str = None,
+    dns_type: str = "ALL",
+    nameserver: str = None,
+    method: str = "udp",
+    timeout: float = 3,
+):
     if nameserver is None:
         nameserver = dns.resolver.BaseResolver().nameservers[0]
     if dns_type == "ALL":
         dns_types = DNS_TYPE
     else:
-        dns_types = [dns_type]
+        dns_types = [dns_type.upper()]
     click.secho(f"Querying {domain} {dns_types} records, from {nameserver}", fg="green")
 
     tasks = []
     for _dns_type in dns_types:
-        task = asyncio.ensure_future(query(nameserver, domain, _dns_type))
-        task.add_done_callback(lambda future: display_result(future.result()))
+        task = asyncio.ensure_future(
+            query(method, nameserver, timeout, domain, _dns_type)
+        )
+        task.add_done_callback(
+            lambda future: click.secho(str(future.exception()), fg="red")
+            if future.exception()
+            else display_result(future.result())
+        )
         tasks.append(task)
-    loop.run_until_complete(asyncio.gather(*tasks))
+    loop.run_until_complete(asyncio.gather(*tasks, return_exceptions=True))
 
 
 if __name__ == "__main__":
